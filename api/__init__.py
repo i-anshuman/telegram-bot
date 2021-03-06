e8ffd40e4a89ddf5bb105f9aa846723d7b702aec
Anshuman Gupta <anshuman844@gmail.com>
2021-03-06 23:00:53 +0530

    feat: Enable CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from urllib.parse import quote
 import requests
 from os import environ
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 #app.config.from_object('config')
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def hello():
   return jsonify({
     "telegram_bot": "Hello There!"
   })
 
 @app.route('/send', methods=['POST'])
+@cross_origin()
 def send_message():
   data = request.get_json()
   send_text = 'https://api.telegram.org/bot' + environ.get('BOT_ID')
